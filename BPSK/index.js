930bd319489825d356df8785a7ea3b3741ae1b48
Aditya DS <adithya18062000@gmail.com>
2021-11-15 00:40:08 +0530

    method rename

--- BPSK/index.js
+++ BPSK/index.js
@@ -5,7 +5,7 @@ const h = new Helpers();
 
 const NO_OF_BITS = 10 ** 6;
 const EB_N0_DB = h.linspace(0, 10, 25e-2);
-const BER_THEORETICAL = h.getTheoreticalBER(EB_N0_DB);
+const BER_THEORETICAL = h.getTheoreticalBERBPSK(EB_N0_DB);
 const Bk = h.randi([0, 1], NO_OF_BITS); // message
 const Xk = Bk.map((bit) => (bit === 0 ? 1 : -1)); // modulation
 const BER = new Array(EB_N0_DB.length).fill(0);
