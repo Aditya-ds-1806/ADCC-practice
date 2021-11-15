ac4135ccaf2ecbfab24dc5d680dc9b9f388d0bb1
Aditya DS <adithya18062000@gmail.com>
2021-11-15 23:15:01 +0530

    added simulation controls

--- BFSK/index.js
+++ BFSK/index.js
@@ -5,28 +5,46 @@ const h = new Helpers();
 
 const NO_OF_BITS = 10 ** 6;
 const EB_N0_DB = h.linspace(0, 10, 25e-2);
-const SER_THEORETICAL = h.getTheoreticalBerBfsk(EB_N0_DB);
-const Bk = h.randi([0, 1], NO_OF_BITS); // message
-const Xk = Bk.map((bit) => (bit === 0 ? [1, 0] : [0, 1])); // modulation
+const BER_THEORETICAL = h.getTheoreticalBerBfsk(EB_N0_DB);
+const BER = new Array(EB_N0_DB.length);
 
-const SER = new Array(EB_N0_DB.length);
+const bfskModulator = () => {
+    const Bk = h.randi([0, 1], NO_OF_BITS); // message
+    const Xk = Bk.map((bit) => (bit === 0 ? [1, 0] : [0, 1])); // modulation
+    for (let i = 0; i < EB_N0_DB.length; i += 1) {
+        const Nk = h.getAWGN(EB_N0_DB[i], [NO_OF_BITS, 2]); // AWGN noise
+        const Yk = new Array(NO_OF_BITS).fill(0).map((_, j) => h.sum(Xk[j], Nk[j]));
+        const bHat = Yk.map(([bit1, bit2]) => {
+            if (bit2 - bit1 <= 0) return 0;
+            return 1;
+        }); // ML decision rule
+        const unchangedBits = bHat.reduce((acc, bit, j) => {
+            // eslint-disable-next-line no-param-reassign
+            if (bit === Bk[j]) acc += 1;
+            return acc;
+        }, 0);
+        BER[i] = 1 - (unchangedBits / NO_OF_BITS);
+    }
+};
 
-for (let i = 0; i < EB_N0_DB.length; i += 1) {
-    const Nk = h.getAWGN(EB_N0_DB[i], [NO_OF_BITS, 2]); // AWGN noise
-    const Yk = new Array(NO_OF_BITS).fill(0).map((_, j) => h.sum(Xk[j], Nk[j]));
-    const bHat = Yk.map(([bit1, bit2]) => {
-        if (bit2 - bit1 <= 0) return 0;
-        return 1;
-    }); // ML decision rule
-    const unchangedBits = bHat.reduce((acc, bit, j) => {
-        // eslint-disable-next-line no-param-reassign
-        if (bit === Bk[j]) acc += 1;
-        return acc;
-    }, 0);
-    SER[i] = 1 - (unchangedBits / NO_OF_BITS);
-}
+h.plot(['Theoretical', 'Simulation'], [EB_N0_DB, BER_THEORETICAL], [EB_N0_DB, BER]);
 
-h.plot(['Theoretical', 'Simulation'], [EB_N0_DB, SER_THEORETICAL], [EB_N0_DB, SER]);
+const simulationData = document.getElementById('simulation');
+const theoreticalData = document.getElementById('theory');
+const spinner = document.querySelector('span.spinner-border');
 
-document.getElementById('simulation').addEventListener('click', () => h.saveData(EB_N0_DB, SER, 'simulation'));
-document.getElementById('theory').addEventListener('click', () => h.saveData(EB_N0_DB, SER_THEORETICAL, 'theory'));
+theoreticalData.addEventListener('click', () => h.saveData(EB_N0_DB, BER_THEORETICAL, 'theory'));
+simulationData.addEventListener('click', () => h.saveData(EB_N0_DB, BER, 'simulation'));
+
+document.getElementById('simulate').addEventListener('click', async () => {
+    spinner.classList.remove('d-none');
+    setTimeout(() => {
+        bfskModulator();
+        h.plot(['Theoretical', 'Simulation'], [EB_N0_DB, BER_THEORETICAL], [EB_N0_DB, BER]);
+        simulationData.disabled = false;
+        theoreticalData.disabled = false;
+        spinner.classList.add('d-none');
+    }, 500);
+});
+
+h.fetchJS('index', 'helpers');
