0624cbd00b64f08485f7f44c9caee754a92abbb6
Aditya DS <adithya18062000@gmail.com>
2021-11-18 23:58:50 +0530

    rename variables

--- MPSK/index.js
+++ MPSK/index.js
@@ -12,21 +12,21 @@ document.getElementById('simulate').addEventListener('click', async () => {
     const spinner = document.querySelector('span.spinner-border');
     const mInput = document.getElementById('M');
     const M = 2 ** Math.floor(Math.log2(Number(mInput.value || 8)));
-    let simData = document.getElementById('simulation');
-    let theoData = document.getElementById('theory');
+    let simulationData = document.getElementById('simulation');
+    let theoreticalData = document.getElementById('theory');
 
     mInput.value = M;
     spinner.classList.remove('d-none');
 
     setTimeout(() => {
         const [SER, SER_THEORETICAL] = MPSK(M, SB_N0_DB, NO_OF_SYMBOLS);
-        theoData = h.clone(theoData);
-        simData = h.clone(simData);
-        theoData.addEventListener('click', () => h.saveData(SB_N0_DB, SER_THEORETICAL, 'theory'));
-        simData.addEventListener('click', () => h.saveData(SB_N0_DB, SER, 'simulation'));
+        theoreticalData = h.clone(theoreticalData);
+        simulationData = h.clone(simulationData);
+        theoreticalData.addEventListener('click', () => h.saveData(SB_N0_DB, SER_THEORETICAL, 'theory'));
+        simulationData.addEventListener('click', () => h.saveData(SB_N0_DB, SER, 'simulation'));
         h.plot(['Theoretical', 'Simulation'], [SB_N0_DB, SER_THEORETICAL], [SB_N0_DB, SER]);
-        simData.disabled = false;
-        theoData.disabled = false;
+        simulationData.disabled = false;
+        theoreticalData.disabled = false;
         spinner.classList.add('d-none');
     }, 500);
 });
